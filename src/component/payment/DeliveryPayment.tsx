a844e174c8591a3fe21b712017665c7602451149
Sergey <eich@tut.by>
2021-02-05 17:10:50 +0300

    добавил ts в "DeliveryPayment"

--- src/component/payment/DeliveryPayment.tsx
+++ src/component/payment/DeliveryPayment.tsx
@@ -1,32 +1,40 @@
 import React from 'react'
 import SectionTitle from '../SectionTitle'
-import {connect} from 'react-redux'
+import {connect, ConnectedProps} from 'react-redux'
 import PaymentCard from './PaymentCard'
 import PaymentCash from './PaymentCash'
 import * as paymentTypes from './paymentTypes'
 import SelectorDeliveryPay from '../selector/SelectorDeliveryPay'
+import {RootState} from '../../store'
 
-const DeliveryPayment = ({paymentType, textForUser}) => {
+// const DeliveryPayment = ({paymentType, textForUser}) => {
+const DeliveryPayment = (props: Props) => {
   return (
     <div>
       <SectionTitle title={`Способ оплаты`} />
       <SelectorDeliveryPay />
-      {paymentType === paymentTypes.paymentByCard && (
-        <PaymentCard text={textForUser} />
+      {props.paymentType === paymentTypes.paymentByCard && (
+        <PaymentCard text={props.textForUser} />
       )}
-      {paymentType === paymentTypes.paymentByCash && (
-        <PaymentCash text={textForUser} />
+      {props.paymentType === paymentTypes.paymentByCash && (
+        <PaymentCash text={props.textForUser} />
       )}
     </div>
   )
 }
 
-const mapState = (state) => {
+const mapState = (state: RootState) => {
   return {
     paymentType: state.selector.selectedPaymentType,
   }
 }
 
-const connector = connect(mapState, null)
+const connector = connect(mapState)
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+type Props = PropsFromRedux & {
+  textForUser: string
+}
 
 export default connector(DeliveryPayment)
