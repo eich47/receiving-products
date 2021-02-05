b9b5da339769847214ab3165dc33350707659088
Sergey <eich@tut.by>
2021-02-05 20:48:46 +0300

    добавил ts "PaymentCash"

--- src/component/payment/PaymentCash.tsx
+++ src/component/payment/PaymentCash.tsx
@@ -2,10 +2,11 @@ import React, {useEffect} from 'react'
 import Phone from './Phone'
 import PaymentInfo from './PaymentInfo'
 import * as paymentActions from './payment.action'
-import {connect} from 'react-redux'
+import {connect, ConnectedProps} from 'react-redux'
 
-const PaymentCash = (props) => {
+const PaymentCash = (props: Props) => {
   useEffect(() => {
+    // @ts-ignore
     props.setPhone(null)
   })
   return (
@@ -20,5 +21,10 @@ const mapDispatch = {
   setPhone: paymentActions.setPhone,
 }
 const connector = connect(null, mapDispatch)
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+type Props = PropsFromRedux & {
+  text: string
+}
 
 export default connector(PaymentCash)
